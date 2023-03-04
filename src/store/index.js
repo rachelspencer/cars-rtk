6570a52905a3ddc555a9d0b3020c9a27d5d18bd3
Rachel Spencer <rachelspencer688@gmail.com>
2023-03-04 16:13:20 -0500

    component boilerplate, store, slices set up

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { 
+    carsReducer, 
+    addCar, 
+    removeCar,
+    changeSearchTerm
+ } from './slices/carsSlice'
+ import { formsReducer, changeName, changeCost, formReducer } from './slices/formSlice'
+
+const store = configureStore({
+    reducer: {
+        cars: carsReducer,
+        form: formReducer
+    }
+});
+
+export {
+    store, 
+    addCar, 
+    removeCar,
+    changeSearchTerm,
+    changeName, 
+    changeCost,
+};
